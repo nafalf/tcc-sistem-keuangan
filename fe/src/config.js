fe4ef57cbe96d69b495363499522fd6dd06ce59c
NaufalR12 <naufal.rafid.mf@gmail.com>
2025-05-31 23:31:27 +0700

    rev2331

--- fe/src/config.js
+++ fe/src/config.js
@@ -1,7 +1,5 @@
 const config = {
-  API_URL:
-    process.env.REACT_APP_API_URL ||
-    "https://projek-akhir-505940949397.us-central1.run.app",
+  API_URL: process.env.REACT_APP_API_URL,
 };
 
 export default config;
