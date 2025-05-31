c9b8990a546c7d9bf979fe3dd71532bee871ab24
NaufalR12 <naufal.rafid.mf@gmail.com>
2025-05-31 22:11:06 +0700

    rev2211

--- be/index.js
+++ be/index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 
 // CORS configuration
 const allowedOrigins = [
-  "http://localhost:3000"
+  "https://fe-projek-akhir-dot-b-08-450916.uc.r.appspot.com/"
 ];
 
 app.use(cookieParser());
@@ -113,7 +113,7 @@ app.use((req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const HOST = "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
